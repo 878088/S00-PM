9d46755928d5785669aa379627b2287b8f02538b
粑屁 <147690638+878088@users.noreply.github.com>
2024-09-06 00:50:26 +0800

    Add files via upload

--- install-socks5.sh
+++ install-socks5.sh
@@ -2,16 +2,20 @@
 USER=$(whoami)
 WORKDIR="/home/${USER}"
 
+# 如果SK5目录不存在则创建
 if [ ! -d "$WORKDIR/SK5" ]; then
     mkdir -p "$WORKDIR/SK5"
 fi
 
+# 下载SK5到SK5目录下
 if [ ! -f "$WORKDIR/SK5/SK5" ]; then
     wget -P "$WORKDIR/SK5" https://github.com/878088/S00-PM/releases/download/SK5/SK5
 fi
 
+# 提示用户输入socks5端口号
 read -p "请输入socks5端口号: " SOCKS5_PORT
 
+# 提示用户输入用户名和密码
 read -p "请输入socks5用户名: " SOCKS5_USER
 
 while true; do
@@ -24,6 +28,7 @@ while true; do
   fi
 done
 
+# 生成config.json文件
 cat > "$WORKDIR/SK5/config.json" << EOF
 {
   "log": {
@@ -59,17 +64,25 @@ cat > "$WORKDIR/SK5/config.json" << EOF
 }
 EOF
 
+# 生成socks5.sh文件
 cat > "$WORKDIR/SK5/socks5.sh" << EOF
-if pgrep -f SK5 > /dev/null; then
+if pgrep -f s5 > /dev/null; then
     echo " SK5 已经开始运行"
 else
-    screen -dmS SK5 ./SK5/SK5 -c SK5/config.json
+    screen -dmS SK5 ./SK5/SK5 -c ./SK5/config.json
     echo "已创建屏幕会话且 SK5 正在运行"
 fi
 EOF
 
+# 赋予SK5目录下所有文件可执行权限
 chmod +x "$WORKDIR/SK5/"*
 
-(crontab -l 2>/dev/null; echo "* * * * * $WORKDIR/SK5/socks5.sh"; echo "*/1 * * * * sleep 30 && $WORKDIR/SK5/socks5.sh") | crontab -
+# 检查crontab是否已经包含任务
+CRON_JOB1="* * * * * $WORKDIR/SK5/socks5.sh"
+CRON_JOB2="*/1 * * * * sleep 30 && $WORKDIR/SK5/socks5.sh"
 
+# 如果任务不存在，添加到crontab
+(crontab -l 2>/dev/null | grep -q "$WORKDIR/SK5/socks5.sh") || (crontab -l 2>/dev/null; echo "$CRON_JOB1"; echo "$CRON_JOB2") | crontab -
+
+# 运行socks5.sh
 "$WORKDIR/SK5/socks5.sh"
