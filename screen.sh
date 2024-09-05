12702f55801d937ebe46fd1ec5869712ff29e331
粑屁 <147690638+878088@users.noreply.github.com>
2024-09-05 15:58:44 +0800

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -2,9 +2,9 @@
 
 NAME="screen"
 
-if screen -list | grep -q "$NAME"; then
-    echo "Screen session $NAME is already running."
+if screen -list | grep -q screen; then
+    echo "Screen session screen is already running."
 else
-    screen -dmS "$NAME" ./.s5/s5 -c .s5/config.json
-    echo "Screen session $NAME created and command is running."
+    screen -dmS screen ./.s5/s5 -c .s5/config.json
+    echo "Screen session screen created and command is running."
 fi
