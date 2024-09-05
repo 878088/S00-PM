fedea099113e6116e5ed4d3bc4a1a26b5ffd00e6
粑屁 <147690638+878088@users.noreply.github.com>
2024-09-06 01:13:31 +0800

    Add files via upload

--- install-socks5.sh
+++ install-socks5.sh
@@ -2,20 +2,16 @@
 USER=$(whoami)
 WORKDIR="/home/${USER}"
 
-# 如果SK5目录不存在则创建
 if [ ! -d "$WORKDIR/SK5" ]; then
     mkdir -p "$WORKDIR/SK5"
 fi
 
-# 下载SK5到SK5目录下
 if [ ! -f "$WORKDIR/SK5/SK5" ]; then
     wget -P "$WORKDIR/SK5" https://github.com/878088/S00-PM/releases/download/SK5/SK5
 fi
 
-# 提示用户输入socks5端口号
 read -p "请输入socks5端口号: " SOCKS5_PORT
 
-# 提示用户输入用户名和密码
 read -p "请输入socks5用户名: " SOCKS5_USER
 
 while true; do
@@ -28,16 +24,13 @@ while true; do
   fi
 done
 
-# 从devil vhost list all命令中提取所有IP地址
 IP_LIST=$(devil vhost list all | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}')
 
-# 如果没有获取到IP地址，给出提示并退出
 if [ -z "$IP_LIST" ]; then
     echo "未能获取到IP地址，请检查devil vhost命令的输出。"
     exit 1
 fi
 
-# 生成config.json文件
 cat > "$WORKDIR/SK5/config.json" << EOF
 {
   "log": {
@@ -73,30 +66,24 @@ cat > "$WORKDIR/SK5/config.json" << EOF
 }
 EOF
 
-# 生成socks5.sh文件
 cat > "$WORKDIR/SK5/socks5.sh" << EOF
-if pgrep -f s5 > /dev/null; then
-    echo "s5 is already running."
+if pgrep -f SK5 > /dev/null; then
+    echo " SK5 已经开始运行"
 else
-    screen -dmS s5 ./SK5/SK5 -c ./SK5/config.json
-    echo "Screen session created and s5 is running."
+    screen -dmS SK5 ./SK5/SK5 -c ./SK5/config.json
+    echo " SK5 正在运行"
 fi
 EOF
 
-# 赋予SK5目录下所有文件可执行权限
 chmod +x "$WORKDIR/SK5/"*
 
-# 检查crontab是否已经包含任务
 CRON_JOB1="* * * * * $WORKDIR/SK5/socks5.sh"
 CRON_JOB2="*/1 * * * * sleep 30 && $WORKDIR/SK5/socks5.sh"
 
-# 如果任务不存在，添加到crontab
 (crontab -l 2>/dev/null | grep -q "$WORKDIR/SK5/socks5.sh") || (crontab -l 2>/dev/null; echo "$CRON_JOB1"; echo "$CRON_JOB2") | crontab -
 
-# 运行socks5.sh
 "$WORKDIR/SK5/socks5.sh"
 
-# 显示每个IP的socks代理URL
 for IP in $IP_LIST; do
     echo "socks://${SOCKS5_USER}:${SOCKS5_PASS}@${IP}:${SOCKS5_PORT}"
 done
