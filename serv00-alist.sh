55c6255f333c00897a297c246fda1562ae3af9a9
粑屁 <147690638+878088@users.noreply.github.com>
2024-09-07 19:37:26 +0800

    Add files via upload

---
+++ serv00-alist.sh
@@ -0,0 +1,110 @@
+#!/bin/sh
+USER=$(whoami)
+WORKDIR="/home/${USER}"
+
+API_URL="https://api.github.com/repos/ykxVK8yL5L/alist/releases/latest"
+
+DOWNLOAD_URL=https://github.com/ykxVK8yL5L/alist/releases/download/latest/alist-freebsd.tar.gz
+curl -L $DOWNLOAD_URL -o alist.tar.gz
+tar -xvf alist.tar.gz
+
+chmod +x alist
+
+if [ ! -d "$WORKDIR/data" ]; then
+    mkdir -p "$WORKDIR/data"
+fi
+
+read -p "请输入 Mysql 用户: " user
+
+read -p "请输入 Mysql 密码: " password
+
+read -p "请输入 Alist 端口: " port
+
+cat > "$WORKDIR/data/config.json" << EOF
+{
+  "force": false,
+  "notify": true,
+  "site_url": "",
+  "cdn": "",
+  "jwt_secret": "cEhhqhqUCUFkMS0z",
+  "token_expires_in": 48,
+  "database": {
+    "type": "mysql",
+    "host": "mysql1.serv00.com",
+    "port": 3306,
+    "user": "$user",
+    "password": "$password.",
+    "name": "$user",
+    "db_file": "data/data.db",
+    "table_prefix": "x_",
+    "ssl_mode": "",
+    "dsn": ""
+  },
+  "meilisearch": {
+    "host": "http://localhost:7700",
+    "api_key": "",
+    "index_prefix": ""
+  },
+  "scheme": {
+    "address": "0.0.0.0",
+    "http_port": $port,
+    "https_port": -1,
+    "force_https": false,
+    "cert_file": "",
+    "key_file": "",
+    "unix_file": "",
+    "unix_file_perm": ""
+  },
+  "temp_dir": "data/temp",
+  "bleve_dir": "data/bleve",
+  "dist_dir": "",
+  "log": {
+    "enable": false,
+    "name": "data/log/log.log",
+    "max_size": 50,
+    "max_backups": 30,
+    "max_age": 28,
+    "compress": false
+  },
+  "delayed_start": 0,
+  "max_connections": 0,
+  "tls_insecure_skip_verify": true,
+  "tasks": {
+    "download": {
+      "workers": 5,
+      "max_retry": 1,
+      "persist_path": "data/tasks/download.json"
+    },
+    "transfer": {
+      "workers": 5,
+      "max_retry": 2,
+      "persist_path": "data/tasks/transfer.json"
+    },
+    "upload": {
+      "workers": 5,
+      "max_retry": 0,
+      "persist_path": "data/tasks/upload.json"
+    },
+    "copy": {
+      "workers": 5,
+      "max_retry": 2,
+      "persist_path": "data/tasks/copy.json"
+    }
+  },
+  "cors": {
+    "allow_origins": [
+      "*"
+    ],
+    "allow_methods": [
+      "*"
+    ],
+    "allow_headers": [
+      "*"
+    ]
+  },
+  "s3": {
+    "enable": false,
+    "port": 5246,
+    "ssl": false
+  }
+EOF  
